f21a619f541342260c59458caeb09b925d6cf128
Marco Oquendo <marcooquendoc@gmail.com>
2022-07-20 05:55:53 -0500

    Add highlight word when scrolling feature

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const about = document.getElementById('aboutRef');
-const contact = document.getElementById('contactRef');
 const hamburger = document.getElementById('hamburger');
 const closeIcon = document.getElementById('close-icon');
+const about = document.getElementById('aboutRef');
+const contact = document.getElementById('contactRef');
 const portfolio = document.getElementById('portfolioRef');
 const nav = document.getElementById('top-navigation-container');
 
@@ -15,15 +15,15 @@ function openMenu() {
 }
 
 function closeMenu() {
-  closeIcon.classList.add('hide');
-  about.classList.add('top-navigation');
-  contact.classList.add('top-navigation');
-  portfolio.classList.add('top-navigation');
   nav.classList.remove('nav');
   about.classList.remove('big-font');
   contact.classList.remove('big-font');
   portfolio.classList.remove('big-font');
   closeIcon.classList.remove('close-icon');
+  closeIcon.classList.add('hide');
+  about.classList.add('top-navigation');
+  contact.classList.add('top-navigation');
+  portfolio.classList.add('top-navigation');
 }
 
 hamburger.addEventListener('click', openMenu);
@@ -32,3 +32,23 @@ about.addEventListener('click', closeMenu);
 contact.addEventListener('click', closeMenu);
 closeIcon.addEventListener('click', closeMenu);
 portfolio.addEventListener('click', closeMenu);
+
+// highlight word when scrolling feature
+const section = document.querySelectorAll('.section');
+const navlinks = document.querySelectorAll('.top-navigation');
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute('id');
+
+    if (offset <= top && top <= offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove('active');
+        document.querySelector(`header nav a[href *= ${id}]`).classList.add('active');
+      });
+    }
+  });
+};
