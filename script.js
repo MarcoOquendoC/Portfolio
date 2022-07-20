031a155dd9bcc8612d836949fa540fdab79a7700
Marco Oquendo <marcooquendoc@gmail.com>
2022-07-20 03:06:57 -0500

    Combine same lines of code in one function x2

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const nav = document.getElementById('top-navigation-container');
-const closeIcon = document.getElementById('close-icon');
-const hamburger = document.getElementById('hamburger');
-const portfolio = document.getElementById('portfolioRef');
 const about = document.getElementById('aboutRef');
 const contact = document.getElementById('contactRef');
+const hamburger = document.getElementById('hamburger');
+const closeIcon = document.getElementById('close-icon');
+const portfolio = document.getElementById('portfolioRef');
+const nav = document.getElementById('top-navigation-container');
 
-hamburger.addEventListener('click', () => {
+function openMenu() {
   closeIcon.classList.remove('hide');
   nav.classList.add('nav');
   about.classList.add('big-font');
   contact.classList.add('big-font');
   portfolio.classList.add('big-font');
   closeIcon.classList.add('close-icon');
-});
+}
 
-closeIcon.addEventListener('click', () => {
+function closeMenu() {
   closeIcon.classList.add('hide');
   about.classList.add('top-navigation');
   contact.classList.add('top-navigation');
@@ -24,40 +24,11 @@ closeIcon.addEventListener('click', () => {
   contact.classList.remove('big-font');
   portfolio.classList.remove('big-font');
   closeIcon.classList.remove('close-icon');
-});
+}
 
-portfolio.addEventListener('click', () => {
-  closeIcon.classList.add('hide');
-  about.classList.add('top-navigation');
-  contact.classList.add('top-navigation');
-  portfolio.classList.add('top-navigation');
-  nav.classList.remove('nav');
-  about.classList.remove('big-font');
-  contact.classList.remove('big-font');
-  portfolio.classList.remove('big-font');
-  closeIcon.classList.remove('close-icon');
-});
+hamburger.addEventListener('click', openMenu);
 
-about.addEventListener('click', () => {
-  closeIcon.classList.add('hide');
-  about.classList.add('top-navigation');
-  contact.classList.add('top-navigation');
-  portfolio.classList.add('top-navigation');
-  nav.classList.remove('nav');
-  about.classList.remove('big-font');
-  contact.classList.remove('big-font');
-  portfolio.classList.remove('big-font');
-  closeIcon.classList.remove('close-icon');
-});
-
-contact.addEventListener('click', () => {
-  closeIcon.classList.add('hide');
-  about.classList.add('top-navigation');
-  contact.classList.add('top-navigation');
-  portfolio.classList.add('top-navigation');
-  nav.classList.remove('nav');
-  about.classList.remove('big-font');
-  contact.classList.remove('big-font');
-  portfolio.classList.remove('big-font');
-  closeIcon.classList.remove('close-icon');
-});
+about.addEventListener('click', closeMenu);
+contact.addEventListener('click', closeMenu);
+closeIcon.addEventListener('click', closeMenu);
+portfolio.addEventListener('click', closeMenu);
